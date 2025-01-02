7a7670bb6564ba983abda35554ad8e31ea0c5028
DuongHoangHiep <duong.hoanghiepp@gmail.com>
2025-01-02 15:47:03 +0700

    update font, UI score

---
+++ assets/Scripts/Controller/UIGameController.ts
@@ -0,0 +1,33 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = UIGameController
+ * DateTime = Thu Jan 02 2025 15:46:36 GMT+0700 (Indochina Time)
+ * Author = hoanghiep2001
+ * FileBasename = UIGameController.ts
+ * FileBasenameNoExtension = UIGameController
+ * URL = db://assets/Scripts/Controller/UIGameController.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
+ *
+ */
+ 
+@ccclass('UIGameController')
+export class UIGameController extends Component {
+    protected start(): void {
+        
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/en/scripting/decorator.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/en/scripting/life-cycle-callbacks.html
+ */
